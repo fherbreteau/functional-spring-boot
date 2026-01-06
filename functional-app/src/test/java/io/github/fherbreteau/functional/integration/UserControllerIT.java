edd7a91021fa4ebe233bcdd7e57cc5681a092cd4
François HERBRETEAU <72505172+fherbreteau@users.noreply.github.com>
2026-01-07 00:06:18 +0100

    fix: Attempt to fix pipeline

--- functional-app/src/test/java/io/github/fherbreteau/functional/integration/UserControllerIT.java
+++ functional-app/src/test/java/io/github/fherbreteau/functional/integration/UserControllerIT.java
@@ -1,7 +1,5 @@
 package io.github.fherbreteau.functional.integration;
 
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -9,10 +7,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.authzed.api.v1.PermissionsServiceGrpc.PermissionsServiceBlockingStub;
-import com.authzed.api.v1.SchemaServiceGrpc.SchemaServiceBlockingStub;
 import io.github.fherbreteau.functional.FunctionalApplication;
-import io.github.fherbreteau.functional.driven.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +17,6 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -36,12 +30,6 @@ class UserControllerIT {
 
     @Autowired
     private WebApplicationContext context;
-    @MockitoBean
-    private UserRepository userRepository;
-    @MockitoBean
-    private PermissionsServiceBlockingStub permissionsService;
-    @MockitoBean
-    private SchemaServiceBlockingStub schemaService;
 
     private MockMvc mvc;
 
@@ -55,13 +43,11 @@ class UserControllerIT {
 
     @WithMockUser
     @Test
-    void fakeTest() throws Exception {
-        when(userRepository.exists(anyString())).thenThrow(new UnsupportedOperationException("Not Implemented Yet"));
+    void listUsersAsNotExistingUser() throws Exception {
         mvc.perform(get("/users").with(csrf()))
-                .andExpect(status().isInternalServerError())
+                .andExpect(status().isBadRequest())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.type").value("UnsupportedOperationException"))
-                .andExpect(jsonPath("$.message").value("Not Implemented Yet"));
-
+                .andExpect(jsonPath("$.type").value("UserException"))
+                .andExpect(jsonPath("$.message").value("user not found"));
     }
 }
