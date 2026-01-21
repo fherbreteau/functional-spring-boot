a9873f2caacf97dcad2081245902241f7333a81d
François HERBRETEAU <72505172+fherbreteau@users.noreply.github.com>
2026-01-21 01:50:58 +0100

    feat: Initialize Spicedb data with root informations

--- functional-domain/src/main/java/io/github/fherbreteau/functional/driving/RuleConfigurator.java
+++ functional-domain/src/main/java/io/github/fherbreteau/functional/driving/RuleConfigurator.java
@@ -2,6 +2,13 @@ package io.github.fherbreteau.functional.driving;
 
 public interface RuleConfigurator {
 
+    /**
+     * Defined the rules.
+     */
     void defineRules();
 
+    /**
+     * Initialize the default user
+     */
+    void initializeDefaultUser();
 }
